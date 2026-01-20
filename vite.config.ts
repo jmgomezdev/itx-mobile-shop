8ab54516fac039ca24b1016ecf3d11f6efe9856d
José Manuel Gómez Pérez <73133693+jmgomezdev@users.noreply.github.com>
2026-01-20 17:17:00 +0100

    chore: :heavy_plus_sign: Added new dependencies for React Query and updated TypeScript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@services": path.resolve(__dirname, "./src/services"),
-      "@types": path.resolve(__dirname, "./src/types"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@context": path.resolve(__dirname, "./src/context"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,24 @@
-import { defineConfig } from "vitest/config";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/core": "/src/core",
-      "@/ui": "/src/ui",
+      '@/': '/src',
     },
   },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/__tests__/setup.ts",
+    environment: 'jsdom',
+    setupFiles: './src/__tests__/setup.ts',
     // environment: "happy-dom",
     coverage: {
-      provider: "v8",
-      include: ["src/**"],
-      exclude: ["src/main.jsx", "src/App.jsx", "**/consts/**", "**/routes/**"],
-      reporter: ["text", "json", "html"],
-      reportsDirectory: "coverage",
+      provider: 'v8',
+      include: ['src/**'],
+      exclude: ['src/main.jsx', 'src/App.jsx', '**/consts/**', '**/routes/**'],
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: 'coverage',
       enabled: true,
     },
   },
