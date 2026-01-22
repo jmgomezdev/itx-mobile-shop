4b593e0a200c80b159c7739d4f9230cbb2e27bab
José Manuel Gómez Pérez <73133693+jmgomezdev@users.noreply.github.com>
2026-01-22 09:39:53 +0100

    chore: :wrench: Updated apiBaseUrl to ensure proper fallback for environment variable

--- src/core/config/env.ts
+++ src/core/config/env.ts
@@ -1,5 +1,7 @@
 const env = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL ?? '',
+  apiBaseUrl:
+    import.meta.env.VITE_API_BASE_URL ??
+    'https://itx-frontend-test.onrender.com',
 } as const;
 
 export { env };
